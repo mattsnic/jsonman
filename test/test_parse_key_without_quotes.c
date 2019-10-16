a3a2f964c389ab4b861f81caba7d3b301dc93a1b
mattsnic <nmattsson@gmail.com>
2019-10-16 17:38:59 +0200

    Add files via upload

---
+++ test/test_parse_key_without_quotes.c
@@ -0,0 +1,78 @@
+#include "test.h"
+
+int main()
+{
+    /*
+     * Testing with one string
+     */
+    char* one_string = "{                            \
+                            \"key1\" : \"value1\"    \
+                        }";
+
+    jsonman_parse(one_string);
+    ERROR_CODE_CHECK;
+
+    int id = 0;
+    do
+    {
+        switch (id)
+        {
+        case 0:
+            ASSERT_EQUALS(get_type(id), JSONMAN_OBJECT);
+            break;
+        case 1:
+            ASSERT_EQUALS(get_type(id), JSONMAN_STRING);
+            break;
+        case 2:
+            ASSERT_EQUALS(get_type(id), JSONMAN_OBJECT_END);
+            break;
+        default:
+            ERROR_AND_RETURN;
+        }
+        id = next_id(id);
+    } while (id > 0);
+
+    jsonman_free();
+    MEM_ALLOC_CHECK;
+
+    /*
+     * Testing with two strings
+     */
+    char* two_strings = "{                            \
+                            \"key1\" : \"value1\",    \
+                            \"key2\" : \"value2\"     \
+                         }";
+
+    jsonman_parse(two_strings);
+    ERROR_CODE_CHECK;
+
+    id = 0;
+    do
+    {
+        switch (id)
+        {
+        case 0:
+            ASSERT_EQUALS(get_type(id), JSONMAN_OBJECT);
+            break;
+        case 1:
+            ASSERT_EQUALS(get_type(id), JSONMAN_STRING);
+            break;
+        case 2:
+            ASSERT_EQUALS(get_type(id), JSONMAN_STRING);
+            break;
+        case 3:
+            ASSERT_EQUALS(get_type(id), JSONMAN_OBJECT_END);
+            break;
+        default:
+            ERROR_AND_RETURN;
+        }
+        id = next_id(id);
+    } while (id > 0);
+
+
+    jsonman_free();
+    MEM_ALLOC_CHECK;
+
+    return 0;
+}
+
