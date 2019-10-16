c315293c4d58d3484af8d7df948fa1d53b6ac305
mattsnic <nmattsson@gmail.com>
2019-10-16 13:34:21 +0200

    Update test.h

--- test/test.h
+++ test/test.h
@@ -1,38 +1,30 @@
 #include "../src/jsonman.h"
 
-#define ERROR do { \
-                    FILE* f; \
-                    fopen_s(&f, "./test_errors.txt", "a"); \
-                    fprintf(f, "Error occured in file %s on line %d\n", __FILE__, __LINE__);  \
-                    fflush(f); \
-                    fclose(f); \
-                 } while(0)
+#define ERROR do                                                                          \
+              {                                                                           \
+                FILE* f;                                                                  \
+                fopen_s(&f, "./test_errors.txt", "a");                                    \
+                fprintf(f, "Error occured in file %s on line %d\n", __FILE__, __LINE__);  \
+                fflush(f);                                                                \
+                fclose(f);                                                                \
+              } while(0)                                                                  \
 
-int mem_alloc_ok()
-{
-    if (MALLOCS <= 0)
-    {
-        ERROR;
-        return 0;
-    }
-    if (FREES <= 0)
-    {
-        ERROR;
-        return 0;
-    }
-    if (MALLOCS != FREES)
-    {
-        ERROR;
-        return 0;
-    }
-    return 1;
-}
+#define ERROR_AND_RETURN do                                                                           \
+                         {                                                                            \
+                            FILE* f;                                                                  \
+                            fopen_s(&f, "./test_errors.txt", "a");                                    \
+                            fprintf(f, "Error occured in file %s on line %d\n", __FILE__, __LINE__);  \
+                            fflush(f);                                                                \
+                            fclose(f);                                                                \
+                            jsonman_free();                                                           \
+                            return 1;                                                                 \
+                         } while(0)                                                                   \
 
-int error_code()
-{
-    if (jsonman_get_last_error() == JSONMAN_NO_ERROR)
-    {
-        return 0;
-    }
-    return 1;
-}
+#define MEM_ALLOC_CHECK if (MALLOCS <= 0 || FREES <= 0 || MALLOCS != FREES)  ERROR_AND_RETURN
+
+#define ERROR_CODE_CHECK if (jsonman_get_last_error() != JSONMAN_NO_ERROR) ERROR_AND_RETURN
+
+#define ASSERT_EQUALS(expected, actual) if (expected != actual) ERROR_AND_RETURN
+
+
+
