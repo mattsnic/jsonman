9bf094ece9733548e728960aaddbd14704e32f49
mattsnic <nmattsson@gmail.com>
2019-10-16 13:33:42 +0200

    Update test_parse_number.c

--- test/test_parse_number.c
+++ test/test_parse_number.c
@@ -5,58 +5,73 @@ int main()
     /*
      * Testing with one number
      */
-    char* single_string = "{                         \
+    char* one_number =    "{                         \
                                 \"int_nr\" : 5       \
                            }";
 
-    jsonman_parse(single_string);
+    jsonman_parse(one_number);
+    ERROR_CODE_CHECK;
+
+    int id = 0;
+    do
+    {
+        switch (id)
+        {
+        case 0:
+            ASSERT_EQUALS(get_type(id), JSONMAN_OBJECT);
+            break;
+        case 1:
+            ASSERT_EQUALS(get_type(id), JSONMAN_NUMBER);
+            break;
+        case 2:
+            ASSERT_EQUALS(get_type(id), JSONMAN_OBJECT_END);
+            break;
+        default:
+            ERROR_AND_RETURN;
+        }
+        id = next_id(id);
+    } while (id > 0);
+
     jsonman_free();
+    MEM_ALLOC_CHECK;
 
-    if (MALLOCS != FREES)
-    {
-        ERROR;
-        return 1;
-    }
-    if (!mem_alloc_ok())
-    {
-        ERROR;
-        return 1;
-    }
-
-    if (error_code())
-    {
-        ERROR;
-        return 1;
-    }
 
     /*
      * Testing with two numbers
      */
-    char* two_strings = "{                     \
+    char* two_numbers = "{                     \
                             \"nr1\" : 1,       \
                             \"nr2\" : 2.34     \
                          }";
 
-    jsonman_parse(two_strings);
+    jsonman_parse(two_numbers);
+    ERROR_CODE_CHECK;
+
+    id = 0;
+    do
+    {
+        switch (id)
+        {
+        case 0:
+            ASSERT_EQUALS(get_type(id), JSONMAN_OBJECT);
+            break;
+        case 1:
+            ASSERT_EQUALS(get_type(id), JSONMAN_NUMBER);
+            break;
+        case 2:
+            ASSERT_EQUALS(get_type(id), JSONMAN_NUMBER);
+            break;
+        case 3:
+            ASSERT_EQUALS(get_type(id), JSONMAN_OBJECT_END);
+            break;
+        default:
+            ERROR_AND_RETURN;
+        }
+        id = next_id(id);
+    } while (id > 0);
+
     jsonman_free();
-
-    if (MALLOCS != FREES)
-    {
-        ERROR;
-        return 1;
-    }
-    if (!mem_alloc_ok())
-    {
-        ERROR;
-        return 1;
-    }
-
-    if (error_code())
-    {
-        ERROR;
-        return 1;
-    }
-
+    MEM_ALLOC_CHECK;
 
     return 0;
 }
