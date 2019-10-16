25c9ca20c941531f2b523fb40e310e2c1347df60
mattsnic <nmattsson@gmail.com>
2019-10-16 17:37:24 +0200

    Update test_parse_string.c

--- test/test_parse_string.c
+++ test/test_parse_string.c
@@ -32,6 +32,24 @@ int main()
         id = next_id(id);
     } while (id > 0);
 
+    size_t key_length;
+    get_key_length(1, &key_length);
+    ASSERT_EQUALS(6, key_length);
+
+    char key[key_length + 1];
+    key[key_length] = '\0';
+    get_key(1, key);
+    ASSERT_STRING_EQUALS("\"key1\"", key);
+
+    size_t value_length;
+    get_value_length(1, &value_length);
+    ASSERT_EQUALS(8, value_length);
+
+    char value[value_length + 1];
+    value[value_length] = '\0';
+    get_value_as_string(1, value);
+    ASSERT_STRING_EQUALS("\"value1\"", value);
+
     jsonman_free();
     MEM_ALLOC_CHECK;
 
@@ -69,6 +87,22 @@ int main()
         id = next_id(id);
     } while (id > 0);
 
+    id = 2;
+    get_key_length(id, &key_length);
+    ASSERT_EQUALS(6, key_length);
+
+    char key2[key_length + 1];
+    key2[key_length] = '\0';
+    get_key(id, key2);
+    ASSERT_STRING_EQUALS("\"key2\"", key2);
+
+    get_value_length(id, &value_length);
+    ASSERT_EQUALS(8, value_length);
+
+    char value2[value_length + 1];
+    value2[value_length] = '\0';
+    get_value_as_string(id, value2);
+    ASSERT_STRING_EQUALS("\"value2\"", value2);
 
     jsonman_free();
     MEM_ALLOC_CHECK;
